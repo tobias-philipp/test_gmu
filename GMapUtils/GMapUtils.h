8b5944c829f3d69a522a5256c0fb9fc669e198b2
Tobias Philipp <t.philipp@xpure.de>
2020-01-20 13:10:43 +0100

    initial commit

---
+++ GMapUtils/GMapUtils.h
@@ -0,0 +1,19 @@
+//
+//  GMapUtils.h
+//  GMapUtils
+//
+//  Created by tphilipp on 20.01.20.
+//  Copyright © 2020 UMI Urban Mobilitsy International GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GMapUtils.
+FOUNDATION_EXPORT double GMapUtilsVersionNumber;
+
+//! Project version string for GMapUtils.
+FOUNDATION_EXPORT const unsigned char GMapUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMapUtils/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GMapUtils' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GMapUtils
+  pod 'GoogleMaps','~> 3.5.0'
+
+  target 'GMapUtilsTests' do
+    # Pods for testing
+  end
+
+end
